b5dedc48af4d4ede08cfe85f77e985b6c4b9de2b
matthewtimmons <Matthwebt@yahoo.com>
2018-06-27 18:05:31 -0500

    Initial commit

---
+++ app/src/main/java/com/teamtreehouse/mememaker/utils/FileUtilities.java
@@ -0,0 +1,74 @@
+package com.teamtreehouse.mememaker.utils;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.teamtreehouse.mememaker.MemeMakerApplicationSettings;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class FileUtilities {
+
+    public static void saveAssetImage(Context context, String assetName) {
+        File fileDirectory = context.getFilesDir();
+        File fileToWrite = new File(fileDirectory, assetName);
+
+        AssetManager assetManager = context.getAssets();
+        try {
+            InputStream in = assetManager.open(assetName);
+            FileOutputStream out = new FileOutputStream(fileToWrite);
+
+            in.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static Uri saveImageForSharing(Context context, Bitmap bitmap,  String assetName) {
+        File fileToWrite = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), assetName);
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream(fileToWrite);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.flush();
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            return Uri.fromFile(fileToWrite);
+        }
+    }
+
+
+    public static void saveImage(Context context, Bitmap bitmap, String name) {
+        File fileDirectory = context.getFilesDir();
+        File fileToWrite = new File(fileDirectory, name);
+
+        try {
+            FileOutputStream outputStream = new FileOutputStream(fileToWrite);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+            outputStream.flush();
+            outputStream.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
